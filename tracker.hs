3ee91955cbc97c36ae2c2897ff73d2ebffdea81f
Will Vining <wfvining@gmail.com>
2013-11-20 14:50:22 -0700

    functional prototype

---
+++ tracker.hs
@@ -0,0 +1,101 @@
+-- Track task completion.
+
+import System.Environment
+import System.IO
+import System.Directory
+
+-- Tracker can do four things:
+-- "add" -> add a task.
+-- "remove" -> remove a task.
+-- "view" -> show details of a task, or a synopsis of all tasks.
+-- "advance" -> note progress on a task.
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [("add", add),
+            ("remove", remove),
+            ("view", view),
+            ("advance", advance),
+            ("ammend", ammend)]
+
+taskToFile :: String -> String
+-- I have serious problems :-)
+taskToFile = ('.':) . flip (++) ".tkr"
+
+-- Add the specified task with the specified target. If the task
+-- already exists (ie the .<taskName>.tkr file is present in the
+-- current directory then no action is taken and a message is
+-- displayed.
+add :: [String] -> IO ()
+add [taskName, target] = do
+  let taskFile = taskToFile taskName
+  exists <- doesFileExist taskFile
+  if exists
+  then putStrLn "Task already exists"
+  else writeFile taskFile (target ++ "\n0\n")
+
+-- remove a task, will prompt the user for confirmation.  A -q option
+-- can be used preceding the name of the task to supress this behavior
+remove :: [String] -> IO ()
+remove [taskName] = do
+  putStrLn $ "Removing task \"" ++ taskName ++ "\" [yes/no]"
+  answer <- getLine
+  if answer == "yes"
+  then removeFile $ taskToFile taskName
+  else putStrLn "Task saved."
+remove ["-q", taskName] = do
+  removeFile $ taskToFile taskName
+
+summarize :: String -> String
+summarize str =
+    (show $ floor $ (cur / target) * 100) ++ "% complete with " 
+                                              ++ (show $ target - cur)
+                                              ++ " tasks remaining."
+        where (tgt:cv:_) = lines str
+              cur        = read cv
+              target     = read tgt
+
+view (taskName:[]) = do
+  let taskFile = taskToFile taskName
+  exists <- doesFileExist taskFile
+  if exists
+  then do 
+    contents <- readFile taskFile
+    putStrLn $ summarize contents
+  else putStrLn ""
+
+-- Very rough and extremely inneficient.
+-- TODO: save comment.
+--       timestamp.
+advance (taskName:amount:comment) = do
+  let tf = taskToFile taskName
+  handle <- openFile tf ReadMode
+  (tempName, tempHandle) <- openTempFile "." (taskName ++ ".temp.tkr")
+  contents <- hGetContents handle
+  let (tgt:cv:updates) = lines contents
+      cur = read cv
+      add = read amount
+  hPutStrLn tempHandle $ unlines $ (tgt:(show (cur+add)):updates) 
+                ++ [(amount ++ " " ++ map (\c -> if c == '\n' 
+                                                 then ' ' 
+                                                 else c) (unlines comment))]
+  hClose handle
+  hClose tempHandle
+  removeFile tf
+  renameFile tempName tf
+
+ammend [taskName,newAmt] = do
+  let tf = taskToFile taskName
+  handle <- openFile tf ReadMode
+  (tempName, tempHandle) <- openTempFile "." (taskName ++ ".temp.tkr")
+  contents <- hGetContents handle
+  let (cur:rest) = lines contents
+  hPutStr tempHandle $ unlines ((newAmt:rest) 
+                                ++ ["0 Ammend " ++ cur ++ "->" ++ newAmt])
+  hClose handle
+  hClose tempHandle
+  removeFile tf
+  renameFile tempName tf
+
+main = do
+  (command:args) <- getArgs
+  let (Just action) = lookup command dispatch
+  action args
